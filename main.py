e6da3b80186cb343bb21404a59985cb855f6ff0a
Rahmat Akintola <akintolarahmah@gmail.com>
2025-09-03 23:39:39 +0000

    add chat api endpoints

--- main.py
+++ main.py
@@ -2,41 +2,46 @@ import os
 import random
 
 import groq
-from groq import Groq
 from dotenv import load_dotenv
+from groq import Groq
+
 load_dotenv()
 
-# set GROQ API KEY from env
 
-GROQ_API_KEY = os.getenv('GROQ_API_KEY')
-groq_client = Groq(api_key = GROQ_API_KEY)
+# set GROQ API KEY from env
+GROQ_API_KEY = os.getenv("GROQ_API_KEY")
+groq_client = Groq(api_key=GROQ_API_KEY)
+
 
 sys_prompt = """You are a helpful virtual assistant. \
     Your goal is to provide useful and relevant \
         responses to my request"""
 
 models = [
-    'qwen/qwen3-32b',
-    'openai/gpt-oss-20b',
-    'llama-3.3-70b-versatile',
-    'gemma2-9b-it'
+    "qwen/qwen3-32b",
+    "openai/gpt-oss-20b",
+    "llama-3.3-70b-versatile",
+    "gemma2-9b-it",
 ]
 
-def generate_message(model, query, temperature=0):
-    response = groq_client.chat.completions.create(
-        messages=[
-            {'role': "system", "content": sys_prompt},
-            {'role': "user", "content": query}
-        ],
-        model=model,
-        response_format={"type": "text"},
-        temperature=temperature
-    )
-    answer = response.choices[0].message.content
-    return answer
 
-if __name__ == '__main__':
+def generate_message(model, query, temperature=0.0):
+    try:
+        response = groq_client.chat.completions.create(
+            messages=[
+                {"role": "system", "content": sys_prompt},
+                {"role": "user", "content": query},
+            ],
+            model=model,
+            temperature=temperature,
+        )
+        return response.choices[0].message.content
+    except Exception as e:
+        return f"Error: {str(e)}"
+
+
+if __name__ == "__main__":
     model = random.choice(models)
-    query = "Explain a noun to a 4 years old and which model are you using"
-    response = generate_message(model, query, temperature=1)
+    query = "What is the capital of France?"
+    response = generate_message(model, query)
     print(response)
